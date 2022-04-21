1d39b2f27cd21fe0876ecba1f65f43d3d1539923
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-04-20 21:38:35 -0500

    Componentes de TODO Machine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from "react";
+import { CreateTodoButton } from "./Components/CreateTodoButton";
+import { TodoCounter } from "./Components/TodoCounter";
+import { TodoItem } from "./Components/TodoItem";
+import { TodoList } from "./Components/TodoList";
+import { TodoSearch } from "./Components/TodoSearch";
 // import "./App.css";
 
+const todos = [
+  { text: "Todo 1", completed: false },
+  { text: "Todo 2", completed: false },
+  { text: "Todo 3", completed: false },
+  { text: "Todo 4", completed: false },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map((todo, index) => (
+          <TodoItem key={index} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export default App;
