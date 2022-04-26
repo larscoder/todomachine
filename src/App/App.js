32717cc32e674bdf70ad6a483e476bcad0bf2e22
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-04-26 08:42:15 -0500

    Composición de componentes sin React Context

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,58 @@
 import React from "react";
-import { TodoProvider } from "../TodoContext";
-import { AppUI } from "./AppUI";
+import { useTodos } from "./useTodos";
+import { TodoCounter } from "../Components/TodoCounter";
+import { TodoSearch } from "../Components/TodoSearch";
+import { TodoList } from "../Components/TodoList";
+import { TodoItem } from "../Components/TodoItem";
+import { CreateTodoButton } from "../Components/CreateTodoButton";
+import { Modal } from "../Components/Modal";
+import { TodoForm } from "../Components/TodoForm";
 
 function App() {
+  const {
+    error,
+    loading,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    searchValue,
+    setSearchValue,
+    totalTodos,
+    completedTodos,
+    setOpenModal,
+    addTodo,
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoCounter totalTodos={totalTodos} completedTodos={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+
+      <TodoList>
+        {error && <p>Desespérate, hubo un error...</p>}
+        {loading && <p>Estamos cargando, no desesperes...</p>}
+        {!loading && !searchedTodos.length && <p>¡Crea tu primer TODO!</p>}
+
+        {searchedTodos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            index={index}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={completeTodo}
+            onDelete={deleteTodo}
+          />
+        ))}
+      </TodoList>
+      {!!openModal && (
+        <Modal>
+          <TodoForm addTodo={addTodo} setOpenModal={setOpenModal} />
+        </Modal>
+      )}
+
+      <CreateTodoButton setOpenModal={setOpenModal} openModal={openModal} />
+    </React.Fragment>
   );
 }
 
