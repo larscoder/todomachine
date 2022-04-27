fd0dc8f16d96ec4307c3b49cc4ac6fed49a57973
Luis Alberto Ruiz Sarmiento <lars55360@gmail.com>
2022-04-27 07:40:22 -0500

    Súper poderes para render props y render functions

--- src/App/App.js
+++ src/App/App.js
@@ -36,10 +36,25 @@ function App() {
         error={error}
         loading={loading}
         searchedTodos={searchedTodos}
+        totalTodos={totalTodos}
+        searchText={searchValue}
         onError={() => <TodosError />}
         onLoading={() => <TodosLoading />}
         onEmptyTodos={() => <EmptyTodos />}
-        render={(todo) => (
+        onEmptySearchResults={(searchText) => (
+          <p>No hay resultados para: {searchText}</p>
+        )}
+        // render={(todo) => (
+        //   <TodoItem
+        //     key={todo.text}
+        //     text={todo.text}
+        //     completed={todo.completed}
+        //     onComplete={() => completeTodo(todo.text)}
+        //     onDelete={() => deleteTodo(todo.text)}
+        //   />
+        // )}
+      >
+        {(todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
@@ -48,7 +63,7 @@ function App() {
             onDelete={() => deleteTodo(todo.text)}
           />
         )}
-      />
+      </TodoList>
       {!!openModal && (
         <Modal>
           <TodoForm addTodo={addTodo} setOpenModal={setOpenModal} />
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./css/TodoList.css";
 
 function TodoList(props) {
+  const renderFunc = props.children || props.render;
+
   return (
     <section className="TodoList-container">
       {props.error && props.onError()}
       {props.loading && props.onLoading()}
-      {!props.loading && !props.searchedTodos.length && props.onEmptyTodos()}
+      {!props.loading && !props.totalTodos && props.onEmptyTodos()}
 
-      <ul>{props.searchedTodos.map(props.render)}</ul>
+      {!!props.totalTodos &&
+        !props.searchedTodos.length &&
+        props.onEmptySearchResults(props.searchText)}
+
+      <ul>{props.searchedTodos.map(renderFunc)}</ul>
     </section>
   );
 }
